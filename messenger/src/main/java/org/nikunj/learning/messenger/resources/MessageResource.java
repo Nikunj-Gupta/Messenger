e0bfdaa5835ed48b8348223c829dc77ffac6ee3a
Nikunj-Gupta <guptanikunj1997@gmail.com>
2016-07-09 15:55:29 +0530

    Updated

--- messenger/src/main/java/org/nikunj/learning/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/nikunj/learning/messenger/resources/MessageResource.java
@@ -1,19 +1,26 @@
 package org.nikunj.learning.messenger.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.nikunj.learning.messenger.model.Message;
+import org.nikunj.learning.messenger.service.MessageService;
+
 @Path("/messages")
 
 public class MessageResource {
 	
+	MessageService messageService = new MessageService();
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessages()
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages()
 	{
-		return "Hello World!";
+		return messageService.getAllMessages();
 	}
 
 }
