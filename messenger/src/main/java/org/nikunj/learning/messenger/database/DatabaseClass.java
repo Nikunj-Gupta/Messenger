901f35faa01b0cde022fd9b6b59f6293b1076dd4
Nikunj-Gupta <guptanikunj1997@gmail.com>
2016-07-09 16:52:48 +0530

    Profile Implemented

---
+++ messenger/src/main/java/org/nikunj/learning/messenger/database/DatabaseClass.java
@@ -0,0 +1,21 @@
+package org.nikunj.learning.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.nikunj.learning.messenger.model.Message;
+import org.nikunj.learning.messenger.model.Profile;
+
+public class DatabaseClass {
+	
+	private static Map<Long, Message> messages = new HashMap();
+	private static Map<Long, Profile> profiles = new HashMap();
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	public static Map<Long, Profile> getProfiles() {
+		return profiles;
+	} 
+
+}
