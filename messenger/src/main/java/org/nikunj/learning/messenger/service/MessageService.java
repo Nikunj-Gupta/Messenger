19d4896e8dd34e143bd269bbcbde4ab61f230146
Nikunj-Gupta <guptanikunj1997@gmail.com>
2016-07-10 22:24:11 +0530

    QueryParam Implemented

--- messenger/src/main/java/org/nikunj/learning/messenger/service/MessageService.java
+++ messenger/src/main/java/org/nikunj/learning/messenger/service/MessageService.java
@@ -1,6 +1,7 @@
 package org.nikunj.learning.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +28,29 @@ public MessageService()
 		list.add(m2);
 		return list;*/
 		return new ArrayList<Message>(messages.values());
+	}
+	
+	public List<Message> getMessagesForYear(int year)
+	{
+		List<Message> messagesForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for(Message message : messages.values())
+		{
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR) == year)
+			{
+				messagesForYear.add(message);
+			}
+		}
+		
+		return messagesForYear;
+	}
+	
+	public List<Message> getMessagesPaginated(int start, int size)
+	{
+		ArrayList<Message> list = new ArrayList<Message>(messages.values());
+		if(start + size > list.size()) return new ArrayList<Message>();
+		return list.subList(start, start + size);
 	}
 	
 	public Message getMessage(long id)
